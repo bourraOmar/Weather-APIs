a26285db7ca186e2b071ca6d834c97419f09d80b
bourraOmar <bwrtmr@gmail.com>
2025-04-18 11:56:29 +0100

    add the view file

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+
+    <main class="flex-1">
+      <Weather />
+    </main>
+
+  </div>
+</template>
+
+<script setup>
+
+import Weather from './components/Weather.vue';
+
+</script>
